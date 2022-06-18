afeaed08fc062470c7901f68668803dab848281f
Shirin Hajahmadi <62059613+shirinhaj@users.noreply.github.com>
2022-06-18 13:30:16 +0200

    FirstUpload

---
+++ Store/Assets/Scripts/RotateTheObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTheObject : MonoBehaviour
+{
+    [SerializeField] private Vector3 _rotation;
+
+    public void Update()
+    {
+        transform.Rotate(_rotation * Time.deltaTime);
+    }
+}
